2c92e91b3a9e369186c923046c3604abe4b8501f
Dustin <walkupd@oregonstate.edu>
2021-02-12 16:12:39 -0800

    set up venv

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+
+# Configuration
+
+app = Flask(__name__)
+
+# Routes
+
+@app.route('/')
+def root():
+    return render_template("main.j2")
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 7766))
+    app.run(port = port, debug=True)
+
+
