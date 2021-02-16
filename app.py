edb214f35e3dbb26e1d20181eaa06d3f00db86bb
Dustin <walkupd@oregonstate.edu>
2021-02-15 18:36:52 -0800

    add student to database functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 import database.db_connector as db
 from database.db_connector import connect_to_database, execute_query
@@ -12,7 +12,7 @@ db_connection = db.connect_to_database()
 
 @app.route('/')
 def root():
-    return render_template("main.j2")
+    return render_template("index.html")
 
 @app.route('/students')
 def students():
@@ -22,6 +22,30 @@ def students():
     result = execute_query(db_connection, query).fetchall();
     print(result)
     return render_template('students.html', rows=result)
+
+@app.route('/addstudent', methods=['POST','GET'])
+def add_student():
+        db_connection = connect_to_database()
+        if request.method == 'GET':
+            query = 'SELECT advisor_id, first_name, last_name FROM advisors'
+            result = execute_query(db_connection,query).fetchall()
+            print(result)
+            return render_template('addstudent.html', advisors = result)
+        elif request.method == 'POST':
+
+            print('Add new student')
+            first_name_input = request.form['first']
+            last_name_input = request.form['last']
+            major_input = request.form['major']
+            advisor_id_input = request.form['advisor']
+            gpa_input = request.form['gpa']
+
+            query = 'INSERT INTO students (first_name, last_name, major, advisor_id, gpa) VALUES (%s,%s,%s,%s,%s)'
+            data = (first_name_input, last_name_input, major_input, advisor_id_input, gpa_input)
+            execute_query(db_connection, query, data)
+            return redirect('/students')
+      
+            
 
 # Listener
 
