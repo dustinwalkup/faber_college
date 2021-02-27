be0aca1b07e40699d5c1a6de5b4fd3fa0f5170a1
Eric Tran <erictrann1@gmail.com>
2021-02-26 20:46:54 -0800

    Add update and delete functions for advisor, classes, and classrooms

--- app.py
+++ app.py
@@ -183,6 +183,37 @@ def add_advisor():
             execute_query(db_connection, query, data)
             return redirect('/advisors')   
 
+@app.route('/updateadvisor/<int:id>', methods=['GET', 'POST'])
+def update_advisor(id):
+    db_connection = connect_to_database()
+    # display existing data
+    if request.method == 'GET':
+        print('Display existing data')
+
+        query = 'SELECT advisor_id, first_name, last_name FROM advisors WHERE advisor_id = %s' %(id)
+        result = execute_query(db_connection, query).fetchone()  
+        print(result)
+        if result == None:
+            return "No such advisor found"
+           
+        return render_template('/updateadvisor.html', advisor = result)
+
+    elif request.method == "POST":
+        print('Post Request')
+        first_name_input = request.form['first']
+        last_name_input = request.form['last']
+        query = 'UPDATE advisors SET first_name = %s, last_name = %s WHERE advisor_id = %s' 
+        data = (first_name_input, last_name_input, id)
+        execute_query(db_connection, query, data)
+        return redirect('/advisors')        
+
+@app.route('/deleteadvisor/<int:id>')
+def delete_advisor(id):
+    db_connection = connect_to_database()
+    query = 'DELETE FROM advisors where advisor_id = %s' %(id)
+    result = execute_query(db_connection, query)
+    return redirect("/advisors")
+
 # classes
 
 @app.route('/classes')
@@ -218,6 +249,63 @@ def add_class():
             execute_query(db_connection, query, data)
             return redirect('/classes') 
 
+@app.route('/updateclass/<int:id>', methods=['GET', 'POST'])
+def update_class(id):
+    db_connection = connect_to_database()
+    # display existing data
+    if request.method == 'GET':
+        print('Display existing data')
+
+        class_query = 'SELECT class_id, class_name, class_subject, instructor_id, classroom_id FROM classes WHERE class_id = %s' %(id)
+        class_result = execute_query(db_connection, class_query).fetchone()
+        
+        if class_result == None:
+            return "No such class found"
+
+        instructor_query = 'SELECT instructor_id, first_name, last_name FROM instructors'
+        instructor_result = execute_query(db_connection, instructor_query).fetchall()
+
+        # determines instructor to display in select drop down menu
+        idx = 0
+        for row in instructor_result:
+            print(row[0])
+            if row[0] == class_result[4]:
+                print('success')
+                break
+            idx = idx + 1
+        
+        classroom_query = 'SELECT classroom_id FROM classrooms'
+        classroom_result = execute_query(db_connection, classroom_query).fetchall()
+
+        # determines classroom to display in select drop down menu
+        idx2 = 0
+        for row in classroom_result:
+            print(row[0])
+            if row[0] == class_result[4]:
+                print('success')
+                break
+            idx2 = idx2 + 1
+        
+        return render_template('/updateclass.html', classres = class_result, instructors = instructor_result, classrooms = classroom_result, instr_idx = idx, clsrm_idx = idx2)
+
+    elif request.method == "POST":
+        print('Post Request')
+        class_name_input = request.form['name']
+        class_subject_input = request.form['subject']
+        instructor_id_input = request.form['instructor']
+        classroom_id_input = request.form['classroom']
+        query = 'UPDATE classes  SET class_name = %s, class_subject = %s, instructor_id = %s, classroom_id = %s WHERE class_id = %s' 
+        data = (class_name_input, class_subject_input, instructor_id_input, classroom_id_input, id)
+        execute_query(db_connection, query, data)
+        return redirect('/classes')
+
+@app.route('/deleteclass/<int:id>')
+def delete_class(id):
+    db_connection = connect_to_database()
+    query = 'DELETE FROM classes where class_id = %s' %(id)
+    result = execute_query(db_connection, query)
+    return redirect("/classes")
+
 # classrooms
 
 @app.route('/classrooms')
@@ -239,9 +327,40 @@ def add_classroom():
             print('Add new classroom')
             capacity_input = request.form['capacity']
             query = 'INSERT INTO classrooms (capacity) VALUES (%s)'
-            data = (capacity_input)
-            execute_query(db_connection, query, [data])
+            data = (capacity_input,)
+            execute_query(db_connection, query, data)
             return redirect('/classrooms') 
+
+@app.route('/updateclassroom/<int:id>', methods=['GET', 'POST'])
+def update_classroom(id):
+    db_connection = connect_to_database()
+    # display existing data
+    if request.method == 'GET':
+        print('Display existing data')
+
+        query = 'SELECT classroom_id, capacity FROM classrooms WHERE classroom_id = %s' %(id)
+        result = execute_query(db_connection, query).fetchone()  
+        print(result)
+        if result == None:
+            return "No such classroom found"
+           
+        return render_template('/updateclassroom.html', classroom = result)
+
+    elif request.method == "POST":
+        print('Post Request')
+        capacity_input = request.form['capacity']
+        print(request.form.get('capacity'))
+        query = 'UPDATE classrooms SET capacity = %s WHERE classroom_id = %s' 
+        data = (capacity_input, id)
+        execute_query(db_connection, query, data)
+        return redirect('/classrooms')        
+
+@app.route('/deleteclassroom/<int:id>')
+def delete_classroom(id):
+    db_connection = connect_to_database()
+    query = 'DELETE FROM classrooms where classroom_id = %s' %(id)
+    result = execute_query(db_connection, query)
+    return redirect("/classrooms")
 
 # classesstudents
 
